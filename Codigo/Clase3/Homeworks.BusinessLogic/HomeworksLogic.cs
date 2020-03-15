9d50b94de91c38685e723fd7fae4864d63bc1f7b
Andrés Bentos <asbentos@hotmail.com>
2020-03-15 13:02:38 -0300

    Agrega Base para Clase 3: Se agrega un proyecto WebApi funcionando con los acceso a datos hardcodeados

---
+++ Codigo/Clase3/Homeworks.BusinessLogic/HomeworksLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Homeworks.DataAccess;
+using Homeworks.Domain;
+using System.Collections.Generic;
+
+namespace Homeworks.BusinessLogic
+{
+    public class HomeworksLogic
+    {
+        private HomeworksRepository homeworksRepository;
+
+        public HomeworksLogic() {
+            homeworksRepository = new HomeworksRepository();
+        }
+
+        public List<Homework> GetHomeworks() {
+            return homeworksRepository.GetHomeworks();
+        }
+    }
+}
---
+++ Codigo/Clase3/Homeworks.WebApi/Controllers/HomeworksController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Homeworks.BusinessLogic;
+using Homeworks.Domain;
+namespace Homeworks.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeworksController : ControllerBase
+    {
+       private HomeworksLogic homeworksLogic;
+
+    public HomeworksController() {
+        homeworksLogic = new HomeworksLogic();
+    }
+
+    // GET api/homeworks
+    [HttpGet]
+    public ActionResult Get()
+    {
+        List<Homework> homeworks = homeworksLogic.GetHomeworks();
+        return Ok(homeworks);
+    }
+    }
+}
